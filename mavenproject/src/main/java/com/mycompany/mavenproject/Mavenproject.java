15700dd214a24fbdeeab3939b79c3936357b9b61
RiyazAntony <antonyriyas18@gmail.com>
2025-02-25 10:54:12 +0530

    Add files via upload

---
+++ mavenproject/src/main/java/com/mycompany/mavenproject/Mavenproject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author IT20
+ */
+public class Mavenproject {
+
+    public static void main(String[] args) {
+        int a=1, b=3;
+        int sum=a+b;
+        System.out.println(sum);
+    }
+}
